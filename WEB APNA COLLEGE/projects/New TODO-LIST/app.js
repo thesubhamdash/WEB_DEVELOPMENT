0dfbee3c3d338e873ad55240724f730b7e0af721
Subham Das <dassubham919@gmail.com>
2026-02-03 11:28:58 +0530

    Redefined To Do List App with JS Functionalities

---
+++ WEB APNA COLLEGE/projects/New TODO-LIST/app.js
@@ -0,0 +1,25 @@
+inp = document.querySelector("input");
+btn = document.querySelector("button");
+ul = document.querySelector("ul");
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let dltBtn = document.createElement("button");
+    dltBtn.innerText = "delete";
+    item.appendChild(dltBtn);
+    dltBtn.classList.add("delete");
+
+    ul.appendChild(item);
+    inp.value="";
+});
+
+//remove only the existing elements, not the newly added!
+let dltbtns = document.querySelectorAll(".delete");
+for (dltBtn of dltbtns){
+    dltBtn.addEventListener("click", function(){
+        let par = this.parentElement;
+        par.remove();
+    });
+}
