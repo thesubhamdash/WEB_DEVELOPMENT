104669e44872cc4097e058bd58b9ef8fd1d77d9a
Subham Kumar Das <dassubham919@gmail.com>
2026-03-12 14:42:17 +0530

    Importing and Installing Packages

--- WEB APNA COLLEGE/BACKEND/math.js
+++ WEB APNA COLLEGE/BACKEND/math.js
@@ -1,6 +1,9 @@
 // module.exports.sum = (a,b) => a+b;
 
-exports.sum = (a,b) => a+b;
-exports.PI = 3.14;
+// exports.sum = (a,b) => a+b;
+// exports.PI = 3.14;
 
 // module.exports = 123;
+
+export const sum = (a,b) => a+b;
+export const PI = 3.14;
--- WEB APNA COLLEGE/BACKEND/script.js
+++ WEB APNA COLLEGE/BACKEND/script.js
@@ -1,6 +1,10 @@
 // const someVal = require("./math");
 // console.log(someVal.sum(2,3));
 
-const info = require("./Fruits");
+// const info = require("./Fruits");
 
-console.log(info);
+// console.log(info);
+
+import {sum,PI} from "./math.js";
+
+console.log(sum(2,3));
