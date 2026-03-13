e6bec1a57b302639aaf8df43603569aa803800f5
Subham Kumar Das <dassubham919@gmail.com>
2026-03-13 20:48:08 +0530

    EJS Templates, serving static files and creating subtemplates

--- WEB APNA COLLEGE/BACKEND/EJSDir/index.js
+++ WEB APNA COLLEGE/BACKEND/EJSDir/index.js
@@ -4,7 +4,13 @@ const app = express();
 
 const port = 8080;
 
-app.set("viewengine", "ejs");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req,res) => {
     res.render("home.ejs");
