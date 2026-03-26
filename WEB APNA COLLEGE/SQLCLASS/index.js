b3fac7bc7ccc67956b4dbf84b4586a12f4fd26ca
Subham Kumar Das <dassubham919@gmail.com>
2026-03-26 13:43:32 +0530

    Inserting bulk fake data using Faker

--- WEB APNA COLLEGE/SQLCLASS/index.js
+++ WEB APNA COLLEGE/SQLCLASS/index.js
@@ -12,15 +12,30 @@ const connection = mysql.createConnection({
 // let q = "INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)";
 // let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
 
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-let users = [["101", "123_newusera", "abc@gmail.com", "abc"],
-             ["102", "123_newuserb", "abcb@gmail.com", "abcb"],
-             ["103", "123_newuserc", "abcc@gmail.com", "abcc"],
-];
 
+
+let getRandomUser = () => {
+    return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.internet.password()
+    ];
+};
+
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let users = [["101", "123_newusera", "abc@gmail.com", "abc"],
+//              ["102", "123_newuserb", "abcb@gmail.com", "abcb"],
+//              ["103", "123_newuserc", "abcc@gmail.com", "abcc"],
+// ];
+
+let data = [];
+for(let i=0; i<100; i++){
+    data.push(getRandomUser());     //100 fake user's data
+}
 
 try{
-    connection.query(q, [users], (err, result) => {
+    connection.query(q, [data], (err, result) => {
         console.log(result);
     });
 } catch(err){
@@ -30,13 +45,13 @@ try{
 connection.end();
 
 
-let getRandomUser = () => {
-    return {
-        id : faker.string.uuid(),
-        username : faker.internet.username(),
-        email : faker.internet.email(),
-        password : faker.internet.password()
-    };
-};
+// let getRandomUser = () => {
+//     return {
+//         id : faker.string.uuid(),
+//         username : faker.internet.username(),
+//         email : faker.internet.email(),
+//         password : faker.internet.password()
+//     };
+// };
 
 // console.log(getRandomUser());
